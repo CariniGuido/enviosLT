84753a0bf2c2df239276d09c228838e5aef90a9b
CariniGuido <103467691+CariniGuido@users.noreply.github.com>
2023-03-16 04:11:06 -0300

    a ver...

--- app.js
+++ app.js
@@ -1,67 +1,85 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const ejs = require('ejs')
+const ejs = require('ejs');
 const mysql = require('mysql2');
+const multer = require('multer');
 
 const app = express();
-const PORT = process.env.PORT || 3306;
-
+const PORT = process.env.PORT || 3300;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// Configuración de Multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+});
+
+const upload = multer({ storage: storage });
+
 app.get('/', function (req, res) {
-    res.render('index');
+  res.render('index');
+});
+
+app.get('/clientes', function (req, res) {
+  res.render('clientes');
+});
+
+app.get('/locales', function (req, res) {
+  res.render('locales');
+});
+
+app.post('/productos', upload.single('imagen'), function (req, res) {
+  const nombre = req.body.nombre;
+  const descripcion = req.body.descripcion;
+  const precio = req.body.precio;
+  const imagen = req.file;
+
+  // Conectarse a la base de datos
+  const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '4321',
+    database: 'enviosLT'
+  });
+
+  // Insertar los datos del formulario en la tabla de productos
+  connection.query('INSERT INTO productos (nombre, descripcion, precio, imagen) VALUES (?, ?, ?, ?)', [nombre, descripcion, precio, imagen.filename], function (err, result) {
+    if (err) throw err;
+    console.log('Producto insertado en la base de datos');
+  });
+
+  // Redirigir al usuario a la página de productos
+  res.redirect('/clientes');
+});
+
+app.get('/clientes', function (req, res) {
+  // Conectarse a la base de datos
+  const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '4321',
+    database: 'enviosLT'
+  });
+
+  // Obtener todos los productos de la tabla
+  connection.query('SELECT * FROM productos', function (err, results) {
+    if (err) throw err;
+
+    // Renderizar la vista de productos con los datos obtenidos
+    res.render('productos', { productos: results });
+  });
+
+  // Cerrar la conexión a la base de datos
+  connection.end();
 });
 
 app.listen(PORT, function () {
-    console.log(`Servidor iniciado en el puerto ${PORT}`);
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
-
-
-
-app.get('/locales', (req, res) => {
-    res.render('locales');
-});
-
-
-app.get('/clientes', function(req, res) {
-    res.render('clientes');
-  });
-
-
-
-
-
-
-
-app.post('/pedidos', function (req, res) {
-    const nombre = req.body.nombre;
-    const direccion = req.body.direccion;
-    const telefono = req.body.telefono;
-    const productos = req.body.productos;
-
-    const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'carin',
-    password: '',
-    database: 'new connection2'
-});
-    connection.query('SELECT * FROM mi_tabla', (err, results) => {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log(results);
-        }
-    });
-    
-    connection.end();
-    // Guardar el pedido en la base de datos
-    // ...
-
-    res.send('¡Pedido recibido!');
-});
-
-
-
